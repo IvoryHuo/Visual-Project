bb9099ac2d4d31dfcd86b94380fddd4273e15502
Ivory Huo <huoda.nus@gmail.com>
2023-03-31 20:20:19 +0800

    add hierachical clustering

--- app.R
+++ app.R
@@ -1,10 +1,15 @@
 #Load R Packages
 ## Add all your packages here
-pacman::p_load('shiny', 'shinydashboard', 'shinythemes', 
-              'sf','tmap',
-              'plotly', 'tidyverse', 'ggstatsplot', 
-              'tools')
-
+# pacman::p_load('shiny', 'shinydashboard', 'shinythemes', 
+#               'sf','tmap',
+#               'plotly', 'tidyverse', 'ggstatsplot', 
+#               'tools')
+pacman::p_load(rgdal, spdep, tmap, sf, 
+ggpubr, cluster, factoextra, NbClust,
+heatmaply, corrplot, tidyverse,psych,
+Hmisc,knitr,kableExtra,ClustGeo,ggiraphExtra,
+plotly,ggstatsplot,tools,
+shiny,shinydashboard,shinythemes,tidyverse)
 
 # Read rds data file
 ## compared with csv, rds save space and have a faster speed to read in data
@@ -17,6 +22,8 @@ pacman::p_load('shiny', 'shinydashboard', 'shinythemes',
 
 #huoda original rds file
 H_origin_dec_wider <-read_rds(file = "data/rds/origin_dec_wider.rds")
+H_cluster_org <-read_rds(file="data/rds/cluster_org.rds")
+H_cluster_des <- read_rds(file="data/rds/cluster_des.rds")
 #prachi_original_rds_file
 
 
@@ -30,11 +37,18 @@ mpbus <- st_read(dsn = "data/geo/busstop",layer = "BusStop")
 mpsz <- st_read(dsn = "data/geo/sz",layer = "MP14_SUBZONE_NO_SEA_PL")
 mppa <- st_read(dsn = "data/geo/pa",layer = "MP14_PLNG_AREA_NO_SEA_PL")
 mpre <- st_read(dsn = "data/geo/region",layer = "MP14_REGION_WEB_PL")
+##huoda geospatial data
+H_sg_1 <- mpsz %>%
+  select(3,16)
+
 
 #Add geospatial dataset below
 ##huoda mpxx data
 H_mpsz_origin_dec <-left_join(mpsz,H_origin_dec_wider, by = c('SUBZONE_N'='SUBZONE_N')) %>%
   select(3,16,17:42)
+
+
+
 ##aish_mpxx_data
 
 
@@ -67,8 +81,8 @@ ui <- navbarPage(
                                       selectInput(inputId = "H_daytype",
                                                   label = "Weekday or Weekend?",
                                                   choices = c("weekday" = "WEEKDAY",
-                                                              "weekend" = "WEEKEND"),
-                                                  selected = "weekday",
+                                                              "weekend/holiday" = "WEEKENDS/HOLIDAY"),
+                                                  selected = c("weekday",'weekend/holiday'),
                                                   multiple = TRUE),
                                       selectInput(inputId = "H_hour",
                                                   label = "Select the hour:",
@@ -122,7 +136,42 @@ ui <- navbarPage(
              tabPanel("Commuter FLow") #note input)d = A_xxx
   ),
   navbarMenu("Cluster Analysis",
-             tabPanel("Hierarchical Clustering"),
+             tabPanel("Hierarchical Clustering",
+                      sidebarLayout(
+                        sidebarPanel(width = 3,  #inputid should be unique, A/H/P_xx
+                                     selectInput(inputId = "H_cluster_daytype",
+                                                 label = "Weekday or Weekend?",
+                                                 choices = c("weekday" = "WEEKDAY",
+                                                             "weekend/holiday" = "WEEKENDS/HOLIDAY"),
+                                                 selected = c("weekday","weend/holiday"),
+                                                 multiple = TRUE),
+                                     selectInput(inputId = "H_cluster_var",
+                                                 label = "Cluster based on variables of",
+                                                 choices = c("population in the area" = "POP",
+                                                             "commuter from/to the area" = "COMMUTER",
+                                                             "business&edu level" = "BUSINESS_EDU",
+                                                             "recreation level" = "RECREATION",
+                                                             "residentail level" = "RESIDENTIAL",
+                                                             "transit level" = "TRANSIT",
+                                                             "others(utility, waterbody etc) level" = "OTHERS"),
+                                                 selected = c("POP","COMMUTER","BUSINESS_EDU","RECREATION","RESIDENTIAL",'TRANSIT','OTHERS'),
+                                                 multiple = TRUE),
+                                  
+                                     sliderInput(inputId = "H_NoClusters",
+                                                 label = "Number of clusters:",
+                                                 min = 2,max=15,
+                                                 value = c(8)),
+                                    
+                        ),
+                        mainPanel(
+                          plotOutput("H_cluster_org",width = "100%", height = 400),
+                          plotOutput("H_cluster_des",width = "100%", height = 400)
+                          #plotOutput("H_corrplot",width = '50%',height = 100)
+                          #plotOutput("H_cluster_elbow",width = '50%',height = 100),
+                        )
+                      )),
+                
+             
              tabPanel("Kmeans Clustering"),
              tabPanel("Skater Clustering"),
              tabPanel("ClustGeo Clustering")),
@@ -141,8 +190,11 @@ ui <- navbarPage(
 server <- function(input, output){
   ##HD Chroloplot
   output$H_mapPlot <- renderTmap({
+    H_mpsz_origin_dec_plot <- H_mpsz_origin_dec %>%
+      filter(DAY_TYPE %in% (input$H_daytype))
+    
     tmap_options(check.and.fix = TRUE) +
-      tm_shape(H_mpsz_origin_dec)+
+      tm_shape(H_mpsz_origin_dec_plot)+
       tm_fill(input$H_hour,
               n = input$H_classes,
               style = input$H_classification,
@@ -151,6 +203,90 @@ server <- function(input, output){
       tm_view(set.zoom.limits = c(11, 14)
       )
   })
+  
+  # output$H_corrplot <- renderPlot({
+  #   cluster_vars.cor = cor(H_cluster_org[,c(input$H_cluster_va)])
+  #   corrplot.mixed(cluster_vars.cor,
+  #                  lower = "ellipse", 
+  #                  upper = "number",
+  #                  tl.pos = "lt",
+  #                  diag = "l",
+  #                  tl.col = "black")
+  # })
+  
+  
+  
+  # output$H_cluster_elbow({
+  #   cluster.vars <- cluster_org %>%
+  #     filter(DAY_TYPE==input$H_cluster_daytype) %>%
+  #     select(c(input$H_cluster_var))
+  #   
+  #   row.names(cluster.vars) <- cluster.vars$SUBZONE_N_ORI
+  #   df_org <- cluster.vars %>%
+  #     select(c(input$H_cluster_var))
+  #   
+  #   df_org_scaled<-scale(df_org)
+  #   
+  #   set.seed(123)
+  #   fviz_nbclust(df_org_scaled, hcut, method = "wss", k.max = 15) + 
+  #     theme_minimal() + 
+  #     ggtitle("Elbow Method to choose number of clusters")
+  # })
+  # 
+  output$H_cluster_org <- renderPlot({
+    #clustering
+    cluster.vars <- H_cluster_org %>%
+      filter(DAY_TYPE %in% input$H_cluster_daytype) %>%
+      select(c(input$H_cluster_var))
+
+    row.names(cluster.vars) <- cluster.vars$SUBZONE_N_ORI
+    df_org <- cluster.vars %>%
+      select(c(input$H_cluster_var))
+
+    df_org_scaled<-scale(df_org)
+
+    proxmat_org <- dist(df_org_scaled, method = 'euclidean')
+    hclust_ward_org <- hclust(proxmat_org, method = 'ward.D')
+    plot(hclust_ward_org, cex = 0.1)
+    rect.hclust(hclust_ward_org,k=input$H_NoClusters,border=c(1:input$H_NoClusters))
+    legend("topright",legend = paste("Cluster",1:input$H_NoClusters),fill = 1:input$H_NoClusters,border = "white",text.font = 10)
+
+    #Plot Map
+    p1 <-cutree(hclust_ward_org,input$H_NoClusters)
+    sz_label <-as.vector(cluster.vars$"SUBZONE_N_ORI")
+    names(p1) <- sz_label
+    plot(H_sg_1,border = 'grey',col = p1,
+         main = 'Parition of Subzone by Hierachical Clustering(Subzone as Origin)')
+    legend("bottomright",legend = paste("c",1:input$H_NoClusters),fill = 1:input$H_NoClusters,border = 'white')
+  })
+  
+  output$H_cluster_des <- renderPlot({
+    #clustering
+    cluster.vars <- H_cluster_des %>%
+      filter(DAY_TYPE %in% input$H_cluster_daytype) %>%
+      select(c(input$H_cluster_var))
+    
+    row.names(cluster.vars) <- cluster.vars$SUBZONE_N_des
+    df_des <- cluster.vars %>%
+      select(c(input$H_cluster_var))
+    
+    df_des_scaled<-scale(df_des)
+    
+    proxmat_des <- dist(df_des_scaled, method = 'euclidean')
+    hclust_ward_des <- hclust(proxmat_des, method = 'ward.D')
+    plot(hclust_ward_des, cex = 0.1)
+    rect.hclust(hclust_ward_des,k=input$H_NoClusters,border=c(1:input$H_NoClusters))
+    legend("topright",legend = paste("Cluster",1:input$H_NoClusters),fill = 1:input$H_NoClusters,border = "white",text.font = 10)
+    
+    #Plot Map
+    p1 <-cutree(hclust_ward_des,input$H_NoClusters)
+    sz_label <-as.vector(cluster.vars$"SUBZONE_N_DES")
+    names(p1) <- sz_label
+    plot(H_sg_1,border = 'grey',col = p1,
+         main = 'Parition of Subzone by Hierachical Clustering (Subzone as Destination)')
+    legend("bottomright",legend = paste("c",1:input$H_NoClusters),fill = 1:input$H_NoClusters,border = 'white')
+  })
+  
 }
 
 
---
+++ module/clustering.R
@@ -0,0 +1,114 @@
+pacman::p_load(rgdal, spdep, tmap, sf, 
+               ggpubr, cluster, factoextra, NbClust,
+               heatmaply, corrplot, tidyverse,psych,
+               Hmisc,knitr,kableExtra,ClustGeo,ggiraphExtra)
+sg_1 <- st_read(dsn = "data/geo/sz",layer = "MP14_SUBZONE_NO_SEA_PL") %>%
+  select(3,16)
+
+regression_data <- read_rds(file = "data/rds/regression_data.rds")
+
+cluster_org <- regression_data %>%
+  select(c(1:4,6,8,10:14)) %>%
+  group_by(DAY_TYPE,SUBZONE_N_ORI) %>%
+  summarise(POP = mean(POP_ORI),
+            COMMUTER = sum(Commuter_Flow),
+            BUSINESS_EDU = mean(BUSINESS_EDU_ORI),
+            OTHERS = mean(OTHERS_ORI),
+            RECREATION = mean(RECREATION_ORI),
+            RESIDENTIAL = mean(RESIDENTIAL_ORI),
+            TRANSIT = mean(TRANSIT_ORI)) %>%
+  ungroup() %>%
+  drop_na(SUBZONE_N_ORI) %>%
+  replace_na(list(x=0))
+cluster_org[is.na(cluster_org)] <- 0
+write_rds(cluster_org,'data/rds/cluster_org.rds')
+
+
+cluster_des <- regression_data %>%
+  select(c(1:3,5,7:8,15:19)) %>%
+  group_by(DAY_TYPE,SUBZONE_N_DES) %>%
+  summarise(POP = mean(POP_DES),
+            COMMUTER = sum(Commuter_Flow),
+            BUSINESS_EDU = mean(BUSINESS_EDU_DES),
+            OTHERS = mean(OTHERS_DES),
+            RECREATION = mean(RECREATION_DES),
+            RESIDENTIAL = mean(RESIDENTIAL_DES),
+            TRANSIT = mean(TRANSIT_DES)) %>%
+  ungroup() %>%
+  drop_na(SUBZONE_N_DES) %>%
+  replace_na(list(x=0))
+cluster_des[is.na(cluster_des)] <- 0
+write_rds(cluster_des,'data/rds/cluster_des.rds')
+
+
+  
+#multivariate analysis
+cluster_vars.cor = cor(cluster_org[,c(3:9)])
+corrplot.mixed(cluster_vars.cor,
+               lower = "ellipse", 
+               upper = "number",
+               tl.pos = "lt",
+               diag = "l",
+               tl.col = "black")
+
+#clustering
+cluster.vars <- cluster_org %>%
+  filter(DAY_TYPE=='WEEKDAY') %>%
+  select(c(2:9))
+
+row.names(cluster.vars) <- cluster.vars$SUBZONE_N_ORI
+df_org <- cluster.vars %>%
+  select(c(2:8))
+
+#scale
+df_org_scaled<-scale(df_org)
+
+#Hierarchical Clustering
+#number of clusters
+set.seed(123)
+fviz_nbclust(df_org_scaled, hcut, method = "wss", k.max = 15) + 
+  theme_minimal() + 
+  ggtitle("Elbow Method")
+
+#clustering
+proxmat_org <- dist(df_org_scaled, method = 'euclidean')
+hclust_ward_org <- hclust(proxmat_org, method = 'ward.D')
+plot(hclust_ward_org, cex = 0.1) 
+rect.hclust(hclust_ward_org,k=8,border=c(1:8)) 
+legend("topright",legend = paste("c",1:8),fill = 1:8,border = "white",text.font = 10)
+
+
+#Plot Map
+p1 <-cutree(hclust_ward_org,8)
+sz_label <-as.vector(cluster.vars$"SUBZONE_N_ORI")
+names(p1) <- sz_label
+plot(sg_1,border = 'grey',col = p1, 
+     main = 'Parition of Subzone by Hierachical Clustering')
+legend("bottomright",legend = paste("c",1:8),fill = 1:8,border = 'white')
+
+
+#Geo spatial Clustering
+sg_sp <- as_Spatial(sg_1)
+sg_nb <- poly2nb(sg_sp)
+sg_nb
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
